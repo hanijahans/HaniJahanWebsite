3a0347212050fc77d80f8348c854113ebfe6935c
HaniehJ <h.jahanshahi@gmail.com>
2025-10-05 19:12:16 -0700

    Integrate MailerLite loader into VitePress

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,6 +4,46 @@ import { useData, useRoute } from 'vitepress'
 import PortfolioGrid from './components/PortfolioGrid.vue'
 import './custom.css'
 
+let mailerLiteScriptLoaded = false
+
+function loadMailerLite() {
+  if (typeof window === 'undefined') return
+
+  if (typeof window.ml === 'function') {
+    window.ml('account', '1722221')
+    mailerLiteScriptLoaded = true
+    return
+  }
+
+  if (mailerLiteScriptLoaded) return
+
+  const w = window
+  const d = document
+  const f = 'ml'
+
+  w[f] =
+    w[f] ||
+    function () {
+      ;(w[f].q = w[f].q || []).push(arguments)
+    }
+
+  const script = d.createElement('script')
+  script.async = 1
+  script.src = 'https://assets.mailerlite.com/js/universal.js'
+  const firstScript = d.getElementsByTagName('script')[0]
+
+  if (firstScript && firstScript.parentNode) {
+    firstScript.parentNode.insertBefore(script, firstScript)
+  } else if (d.head) {
+    d.head.appendChild(script)
+  } else if (d.body) {
+    d.body.appendChild(script)
+  }
+
+  w.ml('account', '1722221')
+  mailerLiteScriptLoaded = true
+}
+
 function DiscordCTA() {
   return h('div', { class: 'discord-cta' }, [
     h('p', { class: 'discord-title' }, '💬 Join the Conversation'),
@@ -46,10 +86,18 @@ export default {
       }
     })
   },
-  enhanceApp({ app }) {
+  enhanceApp({ app, router }) {
     if (typeof DefaultTheme.enhanceApp === 'function') {
       DefaultTheme.enhanceApp({ app })
     }
     app.component('PortfolioGrid', PortfolioGrid)
+        if (typeof window !== 'undefined') {
+      loadMailerLite()
+      if (router && typeof router.onAfterRouteChanged === 'function') {
+        router.onAfterRouteChanged(() => {
+          loadMailerLite()
+        })
+      }
+    }
   }
 }
